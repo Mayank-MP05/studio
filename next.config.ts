6ee09834600b25441cdb55f526020a19cde7323f
Mayank Pachpande <mayank5pande@gmail.com>
2025-09-15 07:30:40 +0000

    Do the SEO related changes
    Meta Tags
    Title Tag: The title is descriptiv

--- next.config.ts
+++ next.config.ts
@@ -29,8 +29,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https_val',
         hostname: 'mayank5pande.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
         port: '',
         pathname: '/**',
       }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,28 @@ const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-space
 const firaCode = Fira_Code({ subsets: ['latin'], variable: '--font-fira-code'});
 
 export const metadata: Metadata = {
-  title: 'Mayank Pachpande',
-  description: 'Portfolio of Mayank Pachpande, Software Development Engineer.',
+  title: 'Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software Development Portfolio',
+  description: 'Software developer with 2+ years of experience working in a fast-paced startups. Having a strong background in full-stack development, and is proficient in a variety of programming languages and technologies like Javascript, Node, Express, MySQL, MongoDB, React, Next JS, AWS, EC2, S3, Codepipeline, Codebuild, Lambda.',
+  keywords: "Mayank_MP5,Mayank_MP05,MP5,Mayank,Mayank Pachpande,Pachpande,mayank5pande,CSE portfolio,SDE,SDE Portfolio,Pune SDE Portfolio,Mayank Pachpande Portfolio,Mayank Pachpande Projects,Mayank,Pachpande,PCCOE,Pune",
+  authors: [{ name: "Mayank Pachpande" }],
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    title: "Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software Development Portfolio",
+    description: "Software developer with 2+ years of experience working in a fast-paced startups. Having a strong background in full-stack development, and is proficient in a variety of programming languages and technologies like Javascript, Node, Express, MySQL, MongoDB, React, Next JS, AWS, EC2, S3, Codepipeline, Codebuild, Lambda.",
+    images: ["https://media.licdn.com/dms/image/C4D03AQEXUmMi3jttZQ/profile-displayphoto-shrink_800_800/0/1659158229535?e=1691020800&v=beta&t=aISqwp9HJWnJ2qbADzgtKfAd6hmtS5CB6fhmt0sMeKA"],
+    locale: "en_IN",
+    url: "https://mayank5pande.com/",
+    siteName: "Mayank Pachpande || Full Stack Developer - Enthusiast - Programmer || Software Development Portfolio",
+  },
+  metadataBase: new URL('https://mayank5pande.com'),
+  alternates: {
+    canonical: '/',
+  },
+  icons: {
+    icon: 'https://mayank-mp05.github.io/favicon.ico',
+    apple: 'https://mayank-mp05.github.io/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +40,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       <head>
+          <meta charSet="utf-8" />
+       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} ${firaCode.variable} font-body antialiased`}>
         <ThemeProvider
           attribute="class"
@@ -34,5 +57,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-    
